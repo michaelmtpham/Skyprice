54a71d4e27171ce74571f48f39af6000966f3b2c
Michael Pham <michaelmtripham@gmail.com>
2025-06-04 22:58:25 -0500

    Redesigned homepage JS logic. Added new functions for loading CSS and loading JS, and coded a bit of the player-search feature.

--- Skyprice/src/player-search.js
+++ Skyprice/src/player-search.js
@@ -1,35 +1,12 @@
-const { invoke } = window.__TAURI__.core;
-
-let API_Key;
-let api_Key_Valid = false;
-
-document.getElementById("api-form").onsubmit = async function(event) {
+document.getElementById("username-form").onsubmit = async function(event){
     event.preventDefault();
-    API_Key = document.getElementById("api-input").value.trim();
-    try {
-        const response = await fetch(`https://api.hypixel.net/status?key=${API_Key}&uuid=7474f749-4898-4941-a50d-e6836ad4616e`);
-        const data = await response.json();
-        if (data.success) {
-            document.getElementById("greet-input").textContent = 'API key valid! You may proceed.';
-            api_Key_Valid = true;
-            localStorage.setItem('APIKey', API_Key);
-        }
-        else {
-            document.getElementById("greet-input").textContent = 'Invalid API key. Please try again.';
-            api_Key_Valid = false;
-        }
-    }
-    catch(error) {
-        document.getElementById("greet-input").textContent = 'Error occurred. Please try again.';
-        api_Key_Valid = false;
-    }
+    await getUUID(document.getElementById("username").value.trim());
 }
 
-document.getElementById('proceedBtn').addEventListener('click', function() {
-    if (api_Key_Valid) {
-        window.location.href = 'homePage.html';
-    }
-    else {
-        document.getElementById('greet-input').textContent = 'You cannot proceed without a valid API key!';
-    }
-})
+async function getUUID(username) {
+    const API_Key = localStorage.getItem('APIKey')
+    const usernameData = await fetch(`https://api.mojang.com/users/profiles/minecraft/${username}`);
+    const usernameJSON = await usernameData.json();
+
+    document.getElementById("username-result").textContent = usernameJSON.id;
+}
