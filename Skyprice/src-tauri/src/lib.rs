9e4f4f58d1061203f638639144186f8cd5ce2945
Michael Pham <michaelmtripham@gmail.com>
2025-06-06 17:29:55 -0500

    Fixed a major bug in the player-search menu. Everytime you clicked search, it refreshed you to the homepage. I also discovered that I can't call the Hypixel API from the JavaScript front-end, and that I should do it from the back-end using Rust and Tauri commands instead.

--- Skyprice/src-tauri/src/lib.rs
+++ Skyprice/src-tauri/src/lib.rs
@@ -1,8 +1,13 @@
+#[tauri::command]
+fn my_custom_command(invoke_message: String) {
+    println!("I was invoked from JavaScript, with this message: {}", invoke_message);
+}
+
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
     tauri::Builder::default()
         .plugin(tauri_plugin_opener::init())
-        .invoke_handler(tauri::generate_handler![])
+        .invoke_handler(tauri::generate_handler![my_custom_command])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
--- Skyprice/src/player-search.js
+++ Skyprice/src/player-search.js
@@ -1,12 +1,25 @@
-document.getElementById("username-form").onsubmit = async function(event){
-    event.preventDefault();
-    await getUUID(document.getElementById("username").value.trim());
-}
+import { invoke } from '@tauri-apps/api/core';
+
+const playerSearchForm = document.querySelector('#username-form');
+
+playerSearchForm.addEventListener('submit', (event) => {
+    event.preventDefault()
+})
+
+
+//document.getElementById("username-submit").onclick = async function(event) {
+//     event.preventDefault();
+//     document.getElementById("username-result").textContent = "test";
+// };
+
+
+
+await getUUID(document.getElementById("username").value.trim());
 
 async function getUUID(username) {
-    const API_Key = localStorage.getItem('APIKey')
-    const usernameData = await fetch(`https://api.mojang.com/users/profiles/minecraft/${username}`);
-    const usernameJSON = await usernameData.json();
+     const API_Key = localStorage.getItem('APIKey')
+     const usernameData = await fetch(`https://api.mojang.com/users/profiles/minecraft/${username}`);
+     const usernameJSON = await usernameData.json();
 
-    document.getElementById("username-result").textContent = usernameJSON.id;
-}
+     document.getElementById("username-result").textContent = usernameJSON.id;
+ }
