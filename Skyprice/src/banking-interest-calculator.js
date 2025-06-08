f038bab60c9e637e6c4774715faf8f8ab0d1c287
Michael Pham <michaelmtripham@gmail.com>
2025-06-07 23:56:50 -0500

    Fixed a terribly annoying bug in the interest calculator with the Balance Goal. I kept thinking that the problem was with the JavaScript code, but it turned out that the problem was within the HTML (Setting my input to type = number. Also added some additional JavaScript logic.

--- Skyprice/src/banking-interest-calculator.js
+++ Skyprice/src/banking-interest-calculator.js
@@ -7,3 +7,48 @@ document.getElementById("price-calculator").addEventListener("click", async even
 
 //----------------------------------------------------------------------------------------------------------------------
 
+const calculate = document.getElementById("calculate");
+const balanceInput = document.getElementById("balance");
+const museumMilestoneInput = document.getElementById("museum-milestone");
+const balanceGoalInput = document.getElementById("balance-goal");
+const result = document.querySelector(".result");
+let selectedTier = null;
+
+document.querySelectorAll(".bank-tier").forEach(button => {
+    button.addEventListener("click", () => {
+        selectedTier = button.textContent.trim();
+        document.querySelectorAll(".bank-tier").forEach(btn=> btn.classList.remove("selected"));
+        button.classList.add("selected");
+    })
+})
+
+calculate.addEventListener("click", event => {
+    result.innerHTML = "";
+
+    const balance = parseFloat(balanceInput.value);
+    const museumMilestone = parseInt(museumMilestoneInput.value);
+    const balanceGoal = parseFloat(balanceGoalInput.value);
+
+    if (isNaN(balance) || balance < 0) {
+        result.innerHTML = "Please enter a valid balance!";
+        return;
+    }
+    if (!(selectedTier)) {
+        result.innerHTML = "Please select a bank tier!";
+        return;
+    }
+    if (isNaN(museumMilestone) || (museumMilestone < 0) || (museumMilestone > 30)) {
+        result.innerHTML = "Please enter a valid Museum Milestone!";
+        return;
+    }
+    if (balanceGoalInput.value.trim() !== '') {
+        if (isNaN(balanceGoal) || balanceGoal <0) {
+            result.innerHTML = "Please enter a valid balance goal!";
+            return;
+        }
+    }
+
+    result.innerHTML = "All entries are valid!";
+
+})
+
