967748aea04f5bda7b3c94a427705ca21e6ff3bb
Michael Pham <michaelmtripham@gmail.com>
2025-06-10 00:57:22 -0500

    Reformatted HTML slightly. Fixed a bug in the CSS with the result div not displaying in the center. Added some JavaScript code.

--- Skyprice/src/minion-profit-calculator.js
+++ Skyprice/src/minion-profit-calculator.js
@@ -9,3 +9,25 @@ document.getElementById("minion-profit-calculator").addEventListener("click", as
 });
 
 //----------------------------------------------------------------------------------------------------------------------
+
+
+//async function getBazaarPrice(productID) {
+//     const response = await fetch('https://api.hypixel.net/skyblock/bazaar');
+//     const data = await response.json();
+//
+//     if ((data.success) && (data.products[]))
+// }
+
+
+
+document.getElementById("calculate").addEventListener("click", async () => {
+    const minionType = document.getElementById("minion-type").value;
+    const tier = parseInt(document.getElementById("minion-tier").value);
+    const fuel = document.getElementById("fuel-type").value;
+    const hopper = document.getElementById("automated-shipping").value;
+    const upgrade1 = document.getElementById("upgrade-1").value;
+    const upgrade2 = document.getElementById("upgrade-2").value;
+
+    const resultBox = document.querySelector(".result");
+    resultBox.innerHTML = "Calculating...";
+})
