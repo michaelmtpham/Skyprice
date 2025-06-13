fabea04704222b35a409e3558bd0c76aba8bf294
Michael Pham <michaelmtripham@gmail.com>
2025-06-13 01:09:08 -0500

    Added HTML and CSS for RNGesus drop simulator.

---
+++ Skyprice/src/rngesus-drop-simulator.js
@@ -0,0 +1,18 @@
+window.addEventListener("DOMContentLoaded", () => {
+    document.getElementById("banking-interest-calculator").addEventListener("click", async () => {
+        window.location.href = 'banking-interest-calculator.html';
+    });
+    document.getElementById("minion-profit-calculator").addEventListener("click", async () => {
+        window.location.href = 'minion-profit-calculator.html';
+    });
+    document.getElementById("skytasks").addEventListener("click", async () => {
+        window.location.href = 'skytasks.html';
+    });
+    document.getElementById("rngesus-drop-simulator").addEventListener("click", async () => {
+        window.location.href = 'rngesus-drop-simulator.html';
+    })
+
+//--------------------------------------------------------------------------------------------------------------
+
+
+})
