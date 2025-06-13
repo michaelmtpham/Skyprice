fd77abfcd7ff036d4c4416e2307475ada763eb16
Michael Pham <michaelmtripham@gmail.com>
2025-06-13 02:26:34 -0500

    Almost finished code for rngesus-drop-simulator.js

--- Skyprice/src/rngesus-drop-simulator.js
+++ Skyprice/src/rngesus-drop-simulator.js
@@ -14,5 +14,77 @@ window.addEventListener("DOMContentLoaded", () => {
 
 //--------------------------------------------------------------------------------------------------------------
 
+    document.getElementById("simulate").addEventListener("click", () => {
+        const dropName = document.getElementById("drop-name").value.trim();
+        const dropChance = parseFloat(document.getElementById("drop-chance").value);
+        const simulations = parseInt(document.getElementById("simulations").value);
 
+        const result = document.querySelector(".result");
+        result.innerHTML = "";
+
+        if (!dropName) {
+            result.innerHTML = "Please enter a valid name!";
+            return;
+        }
+
+        if (isNaN(dropChance) || dropChance > 100 || dropChance < 0) {
+            result.innerHTML = "Please enter a valid drop chance!";
+            return;
+        }
+
+        if (isNaN(simulations)) {
+            result.innerHTML = "Please enter a valid number of simulations!";
+            return;
+        }
+
+        const probability = dropChance/100;
+        let successfulDrops = 0;
+        let longestDryStreak = 0;
+        let currentDryStreak = 0;
+        const expectedDrops = (probability * simulations).toFixed(2);
+
+
+        for (let i = 0; i < simulations; i++) {
+            if (Math.random() < probability) {
+                successfulDrops++;
+                if (currentDryStreak > longestDryStreak) {
+                    longestDryStreak = currentDryStreak;
+                }
+                currentDryStreak = 0;
+            }
+            else {
+                currentDryStreak++;
+            }
+        }
+
+        let emoji = "😐";
+        const diff = successfulDrops - expectedDrops;
+
+        if (diff >= 10) {
+            emoji = "🤩 OMG WOW SO LUCKY 🤩";
+        }
+        else if (diff <= -10) {
+            emoji = "💀 Cooked 💀";
+        }
+        else if (diff >= 3) {
+            emoji = "😊 YAY YAY YAY YAY YAY 😊";
+        }
+        else if (diff <= -3) {
+            emoji = "😓 gg bro";
+        }
+
+        const percentage = ((successfulDrops / simulations) * 100).toFixed(2);
+
+        result.innerHTML = `
+            <p>Drop: ${dropName}</p>
+            <p>Drop Rate: ${dropChance}</p>
+            <p>Simulations: ${simulations}</p>
+            <p>Expected Drops: ${expectedDrops}</p>
+            <p>Drops Obtained: ${successfulDrops} (${percentage}%) </p>
+            <p>Longest Dry Streak: ${longestDryStreak}</p>
+            <p>RNGesus Verdict: ${emoji}</p>
+        `;
+    })
 })
+
+
