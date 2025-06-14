1afd9f82f8b42a25279b651f20c4dd8cf7195678
Michael Pham <michaelmtripham@gmail.com>
2025-06-13 22:38:55 -0500

    After coding the Kuudra for a bit, I realized it was a bit too straightforward and I decided to change it to a dungeon chest comparator. You enter your inputs, and then it calls the Bazaar API and gets the prices and return. I also changed some CSS stuff.

--- Skyprice/src/dungeon-chest-comparator.js
+++ Skyprice/src/dungeon-chest-comparator.js
@@ -13,6 +13,17 @@ document.getElementById("skytasks").addEventListener("click", async () => {
 document.getElementById("rngesus-drop-simulator").addEventListener("click", async () => {
     window.location.href = 'rngesus-drop-simulator.html';
 })
-document.getElementById("kuudra-key-checker").addEventListener("click", async () => {
-    window.location.href = 'kuudra-key-checker.html';
+document.getElementById("dungeon-chest-comparator").addEventListener("click", async () => {
+    window.location.href = 'dungeon-chest-comparator.html';
 })
+
+//--------------------------------------------------------------------------------------------------
+
+const chestCost = parseFloat(document.getElementById("chest-cost").value);
+const input1 = parseFloat(document.getElementById("input-1").value);
+const input2 = parseFloat(document.getElementById("input-2").value);
+const input3 = parseFloat(document.getElementById("input-3").value);
+const input4 = parseFloat(document.getElementById("input-4").value);
+const input5 = parseFloat(document.getElementById("input-5").value);
+const resultDiv = document.getElementById("result");
+
--- Skyprice/src/flip-profit-calculator.js
+++ Skyprice/src/flip-profit-calculator.js
@@ -11,8 +11,8 @@ window.addEventListener("DOMContentLoaded", () => {
     document.getElementById("rngesus-drop-simulator").addEventListener("click", async () => {
         window.location.href = 'rngesus-drop-simulator.html';
     })
-    document.getElementById("kuudra-key-checker").addEventListener("click", async () => {
-        window.location.href = 'kuudra-key-checker.html';
+    document.getElementById("dungeon-chest-comparator").addEventListener("click", async () => {
+        window.location.href = 'dungeon-chest-comparator.html';
     })
 
 //--------------------------------------------------------------------------------------------------------------
