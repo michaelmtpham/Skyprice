8ca624991a3428efa81a6bbde425708c2a1eaaf4
Michael Pham <michaelmtripham@gmail.com>
2025-06-18 02:19:02 -0500

    Removed all the useless asynchronous junk when redirecting to other feature HTML pages.

--- Skyprice/src/skytasks.js
+++ Skyprice/src/skytasks.js
@@ -1,22 +1,22 @@
-document.getElementById("banking-interest-calculator").addEventListener("click", async () => {
+document.getElementById("banking-interest-calculator").addEventListener("click", () => {
     window.location.href = 'banking-interest-calculator.html';
 });
-document.getElementById("minion-profit-calculator").addEventListener("click", async () => {
+document.getElementById("minion-profit-calculator").addEventListener("click", () => {
     window.location.href = 'minion-profit-calculator.html';
 });
-document.getElementById("flip-profit-calculator").addEventListener("click", async () => {
+document.getElementById("flip-profit-calculator").addEventListener("click", () => {
     window.location.href = 'flip-profit-calculator.html';
 });
-document.getElementById("skytasks").addEventListener("click", async () => {
+document.getElementById("skytasks").addEventListener("click", () => {
     window.location.href = 'skytasks.html';
 });
-document.getElementById("rngesus-drop-simulator").addEventListener("click", async () => {
+document.getElementById("rngesus-drop-simulator").addEventListener("click", () => {
     window.location.href = 'rngesus-drop-simulator.html';
 })
-document.getElementById("dungeon-chest-comparator").addEventListener("click", async () => {
+document.getElementById("dungeon-chest-comparator").addEventListener("click",() => {
     window.location.href = 'dungeon-chest-comparator.html';
 })
-document.getElementById("player-search").addEventListener("click", async () => {
+document.getElementById("player-search").addEventListener("click",() => {
     window.location.href = 'player-search.html';
 })
 
